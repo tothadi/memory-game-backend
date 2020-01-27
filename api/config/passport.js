f826556e496adc77d6a378b1ab46c753276e5031
tothadi <Oakerator66>
2020-01-27 09:17:21 +0100

    error

--- api/config/passport.js
+++ api/config/passport.js
@@ -12,13 +12,13 @@ passport.use(new LocalStrategy({
       // Return if user not found in database
       if (!user) {
         return done(null, false, {
-          message: 'User not found'
+          message: 'User not found!'
         });
       }
       // Return if password is wrong
       if (!user.validPassword(password)) {
         return done(null, false, {
-          message: 'Password is wrong'
+          message: 'Wrong password!'
         });
       }
       // If credentials are correct, return the user object
